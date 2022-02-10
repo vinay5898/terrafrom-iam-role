824c5a473759e87757b363635e7d283d6db045f3
root <root@ip-172-31-7-238.ap-south-1.compute.internal>
2022-02-10 15:42:33 +0000

    no

---
+++ main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+#Create security group with firewall rules
+resource "aws_security_group" "security_jenkins_grp" {
+  name        = var.security_group
+  description = "security group for jenkins"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = var.security_group
+  }
+}
+
+resource "aws_instance" "myFirstInstance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  security_groups= [var.security_group]
+  tags= {
+    Name = var.tag_name
+  }
+}
+
+# Create Elastic IP address
+resource "aws_eip" "myElasticIP" {
+  vpc      = true
+  instance = aws_instance.myFirstInstance.id
+tags= {
+    Name = "testing_terraform_elastic_ip"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+       description = "The AWS region to create things in." 
+       default     = "ap-south-1" 
+}
+
+variable "instance_type" { 
+    description = "instance type for ec2" 
+    default     =  "t2.micro" 
+}
+
+variable "security_group" { 
+    description = "Name of security group" 
+    default     = "my-jenkins-security-group" 
+}
+
+variable "tag_name" { 
+    description = "Tag Name of for Ec2 instance" 
+    default     = "my-ec2-instance" 
+} 
+variable "ami_id" { 
+    description = "AMI for Ubuntu Ec2 instance" 
+    default     = "ami-0851b76e8b1bce90b" 
+}
